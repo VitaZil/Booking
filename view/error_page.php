fe5c10ef3271276dee0134bc016b690c0b12087e
VitaZil <vita.bun@gmail.com>
2022-10-26 12:17:54 +0300

    add error page

---
+++ view/error_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/../resources/style.css">
+    <title>Error</title>
+</head>
+<nav>
+    <?php require(__DIR__ . '/navigation.php') ?>
+</nav>
+<body class="error fixed-home">
+<h1>PAGE NOT FOUND 😥</h1>
+</body>
+</html>
+
